a5759faebf3f9ffa5969ae0876091a31000b165d
diego <diego.scavalcanti@gmail.com>
2024-03-02 12:20:00 -0300

    ultimas partes do body feita, faltando deixar responsivo

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -147,6 +147,112 @@ export const Body = () => {
             </div>
           </Artigo>
         </Sessao>
+        <Sessao>
+          <Artigo>
+            <Card>
+              <img src={Photos.imgRelatório} alt="" />
+              <span>Relatório</span>
+              <SubTitulo variant="sublime-titulo">Leia nosso último relatório da Pesquisa State of Sales.</SubTitulo>
+              <p>Saiba como mais de 7.700 profissionais de vendas estão obtendo sucesso imediato.</p>
+              <Link>Leia o relatório</Link>
+            </Card>
+            <Card>
+              <img src={Photos.astroBlog} alt="" />
+              <span>Blog</span>
+              <SubTitulo variant="sublime-titulo">Conheça o Blog da Salesforce.</SubTitulo>
+              <p>Notícias e percepções para impulsionar a transformação de seus negócios.</p>
+              <Link>Saiba mais</Link>
+            </Card>
+            <Card>
+              <img src={Photos.blogSalesforce} alt="" />
+              <span>Webinar</span>
+              <SubTitulo variant="sublime-titulo">Salesforce e inter: Faça mais com menos.</SubTitulo>
+              <p>Aprenda como conectar os dados do cliente para impulsionar a eficiência do marketing.</p>
+              <Link>Participe do webinar</Link>
+            </Card>
+          </Artigo>
+        </Sessao>
+        <Sessao>
+          <Titulo>Participe de eventos virtuais da Salesforce. Agora a inspiração vem até você.</Titulo>
+          <Artigo>
+            <Card>
+              <img src={Photos.eventos} alt="" />
+              <SubTitulo>Mergulhe em paixão, aprendizado e conhecimento.</SubTitulo>
+              <Link>Explore os eventos</Link>
+            </Card>
+            <Card>
+              <img src={Photos.customer} alt="" />
+              <SubTitulo>Aprenda tudo sobre o Customer 360 em workshops práticos.</SubTitulo>
+              <Link>Confira nossos workshops (Inglês)</Link>
+            </Card>
+            <Card>
+              <img src={Photos.webinars} alt="" />
+              <SubTitulo>Descubra como impulsionar o crescimento com o Salesforce gratuitamente.</SubTitulo>
+              <Link>Assista os Webinars</Link>
+            </Card>
+          </Artigo>
+        </Sessao>
+        <Sessao>
+          <div className="wrappen">
+            <Titulo>Aprenda habilidades valiosas gratuitamente com o Trailhead.</Titulo>
+            <p>Siga os caminhos de aprendizado guiados e aprenda na prática as habilidades interpessoais, digitais e do Salesforce de qualquer lugar.</p>
+          </div>
+          <Artigo>
+            <Card>
+              <img src={Photos.iconeCalen} alt="" />
+              <SubTitulo>Noções básicas de inteligência artificial</SubTitulo>
+              <p>Aprenda as noções básicas de IA e a tecnologia por trás dos seus recursos incríveis.</p>
+              <Link>Comece a aprender</Link>
+            </Card>
+            <Card>
+              <img src={Photos.iconeCell} alt="" />
+              <SubTitulo>Noções básicas do Salesforce Einstein</SubTitulo>
+              <p>Descubra insights e preveja resultados com esse avançado conjunto de recursos aprimorados de IA.</p>
+              <Link>Comece a aprender</Link>
+            </Card>
+            <Card>
+              <img src={Photos.iconeMedalha} alt="" />
+              <SubTitulo>IA generativa do Einstein: início rápido</SubTitulo>
+              <p>Descubra como a IA generativa e o Einstein estão transformando o futuro do trabalho.</p>
+              <Link>Comece a aprender</Link>
+            </Card>
+          </Artigo>
+        </Sessao>
+        
+        <Sessao>
+          <div className="wrappen">
+            <Titulo>Experimente o Salesforce gratuitamente. Não é necessário cartão de crédito nem instalação de software.</Titulo>
+            <Button variant="blue">Comece o teste gratuito</Button>
+            <img src={Photos.freeTest} alt="" />
+          </div>
+        </Sessao>
+        <Sessao>
+          <Card variant="big-card">
+            <Titulo>Perguntas? Vamos colocá-lo no caminho certo.</Titulo>
+            <Button variant="blue">Entre em contato</Button>
+            <img src={Photos.imgQuests} alt="" />
+          </Card>
+        </Sessao>
+        <Sessao>
+          <Titulo>Encontre aplicativos e experiência para estender o Salesforce.</Titulo>
+          <Artigo>
+            <Card>
+              <img src={Photos.appExchange} alt="" />
+              <SubTitulo>O que é AppExchange?</SubTitulo>
+              <Link>Descubra o AppExchange (inglês)</Link>
+            </Card>
+            <Card>
+              <img src={Photos.allApps} alt="" />
+              <SubTitulo>Encontre todos os apps</SubTitulo>
+              <Link>Explore todos os apps (Inglês)</Link>
+            </Card>
+            <Card>
+              <img src={Photos.consultor} alt="" />
+              <SubTitulo>Obtenha orientação especializada.</SubTitulo>
+              <Link>Encontre consultores (inglês)</Link>
+            </Card>
+          </Artigo>
+        </Sessao>
 
       </div>
     </body>
--- src/components/Photos.tsx
+++ src/components/Photos.tsx
@@ -20,6 +20,21 @@ import financeiro from "../assets/financeiro.webp";
 import saude from "../assets/saude.webp";
 import bgRosa from "../assets/bg-rosa.webp";
 import whatsCRM from "../assets/whats-CRM.webp";
+import blogSalesforce from "../assets/salesforce-inter.webp";
+import astroBlog from "../assets/astro-blog.webp";
+import imgRelatório from "../assets/img-relatório.webp";
+import bgVerde from "../assets/bg-verde.webp";
+import eventos from "../assets/eventos.webp";
+import customer from "../assets/customer.webp";
+import webinars from "../assets/webinars.webp";
+import iconeMedalha from "../assets/icone-medalhas.webp";
+import iconeCalen from "../assets/icone-calend.webp";
+import iconeCell from "../assets/icone-cell.webp";
+import freeTest from "../assets/free-test.webp"
+import imgQuests from "../assets/img-quest.jpg";
+import consultor from "../assets/consultor.webp";
+import appExchange from "../assets/app-exchange.webp";
+import allApps from "../assets/all-apps.webp";
 
 export const Photos = {
     appGuide,
@@ -43,5 +58,20 @@ export const Photos = {
     financeiro,
     saude,
     bgRosa,
-    whatsCRM
+    whatsCRM,
+    blogSalesforce,
+    astroBlog,
+    imgRelatório,
+    bgVerde,
+    eventos,
+    customer,
+    webinars,
+    iconeMedalha,
+    iconeCalen,
+    iconeCell,
+    freeTest,
+    imgQuests,
+    consultor,
+    appExchange,
+    allApps
 }
