0fec8a629c355bc4c132423bd246618d1fa20c2c
vitor <vitor52a1@gmail.com>
2024-04-01 20:28:55 -0300

    Astro guia completo

--- src/components/ContainerAstro/ContainerAstro.style.ts
+++ src/components/ContainerAstro/ContainerAstro.style.ts
@@ -3,18 +3,32 @@ import styled from "styled-components";
 export const ContainerAstroElement = styled.div`
 
 &.primary {
-
+  display: flex;
   position: absolute;
   z-index: 20;
-  width: 300px; 
+  max-width: 450px;
   margin-left: 50px;
-  margin-top: 50px;
+  margin-top: 80px;
 }
 
+  &.primary a {
+    text-decoration: none;
+  }
   &.right {
     right: 0;
   }
   &.left {
     left: 0;
+  }
+
+  @media(min-width:375px) {
+    &.primary {
+      margin-top: 350px;
+    }
+  }
+  @media(min-width:1200px) {
+    &.primary {
+      margin-top: 80px;
+    }
   }
 `
